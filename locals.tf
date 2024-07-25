c1cb887b4017e1582ce9a7f1e797688ebfa41dbe
argoyle <joakim@unbound.se>
2024-07-25 00:56:29 +0000

    chore: Bump Operator module version
    Made with ❤️️ by updatecli

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  version       = "0.41.1"
+  version       = "0.42.0"
   agent_version = "0.42.0"
   yaml = templatefile("${path.module}/custom-resources.yaml.tmpl", {
     cluster_name         = var.cluster_name
